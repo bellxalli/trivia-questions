83fbbca2057c2bdb3cf7ca98eb5681cb2655eff4
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-09-23 14:42:06 -0700

    added more js for created elements

--- script.js
+++ script.js
@@ -1,45 +1,73 @@
 window.onload = function()
 {
-    const beginButton = document.getElementById("beginButton");
-    beginButton.onclick = () =>
-    {
-        let randomNum = Math.floor(Math.random() * 4) + 1;
-        //use randomNum to select a question randomly from question array
-    }
-
-
     const questions = [
         {
             question: "What is the biggest sauropod?",
-            answerA: "Argentinosaurus",
-            answerB: "Blue Whale",
-            answerC: "Gignanototsarus"
+            answerCorrect: "Argentinosaurus",
+            answer1: "Blue Whale",
+            answer2: "Gignanototsarus"
         },
         {
             question: "What was the first implemented coding language?",
-            answerA: "Plankalkül",
-            answerB: "Autocode",
-            answerC: "Fortran"
+            answerCorrect: "Plankalkül",
+            answer1: "Autocode",
+            answer2: "Fortran"
         },
         {
             question: "What is the biggest bug right now?",
-            answerA: "My Coding",
-            answerB: "Giant Weta",
-            answerC: "The Common Cold"
+            answer1: "My Coding",
+            answerCorrect: "Giant Weta",
+            answer2: "The Common Cold"
         },
         {
             question: "How many islands in the world?",
-            answerA: "670,000",
-            answerB: "660,000",
-            answerC: "50,000"
+            answerCorrect: "670,000",
+            answer1: "660,000",
+            answer1: "50,000"
         },
         {
             question: "When was the first video game created?",
-            answerA: "1958",
-            answerB: "1972",
-            answerC: "1966"
+            answerCorrect: "1958",
+            answer1: "1972",
+            answer2: "1966"
         }        
     ]
+    
+    const beginButton = document.getElementById("beginButton");
+    beginButton.onclick = () =>
+    {
+        let randomNum = Math.floor(Math.random() * (questions.length-1)) + 1;
+        //use randomNum to select a question randomly from question array
+
+        const questionArea = document.querySelector("#questionArea");
+        const question = document.createElement("h2");
+        question.innerHTML = questions[randomNum].question
+
+        const answerCorrect = document.createElement("button");
+        answerCorrect.className = "correct";
+        answerCorrect.innerHTML = questions[randomNum].answerCorrect
+
+        const answer1 = document.createElement("button");
+        answer1.className = "incorrect";
+        answer1.innerHTML = questions[randomNum].answer1
+
+        const answer2 = document.createElement("button");
+        answer2.className = "incorrect";
+        answer2.innerHTML = questions[randomNum].answer2
+
+        questionArea.appendChild(question);
+        questionArea.appendChild(answer1);
+        questionArea.appendChild(answer2);
+        questionArea.appendChild(answerCorrect);
+
+        // check if answer is correct from onlcick
+        // use another function?
+    }
+
+
+
+
+
     
 
 }
