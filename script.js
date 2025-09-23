61de59ad0b0fce62180b82d83b7eeeca08041e9e
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-09-23 14:18:55 -0700

    deleted js file that was no longer needed

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-import express from 'express';
-
-const app = express();
-
-app.use(express.static('public'));
-
-app.use(express.urlencoded({extended: true}));
-
-let score = [];
-
+window.onload = function()
+{
+    const beginButton = document.getElementById("beginButton");
+    beginButton.onclick = () =>
+    {
+        let randomNum = Math.floor(Math.random() * 4) + 1;
+        //use randomNum to select a question randomly from question array
+    }
 
 
+    const questions = [
+        {
+            question: "What is the biggest sauropod?",
+            answerA: "Argentinosaurus",
+            answerB: "Blue Whale",
+            answerC: "Gignanototsarus"
+        },
+        {
+            question: "What was the first implemented coding language?",
+            answerA: "Plankalkül",
+            answerB: "Autocode",
+            answerC: "Fortran"
+        },
+        {
+            question: "What is the biggest bug right now?",
+            answerA: "My Coding",
+            answerB: "Giant Weta",
+            answerC: "The Common Cold"
+        },
+        {
+            question: "How many islands in the world?",
+            answerA: "670,000",
+            answerB: "660,000",
+            answerC: "50,000"
+        },
+        {
+            question: "When was the first video game created?",
+            answerA: "1958",
+            answerB: "1972",
+            answerC: "1966"
+        }        
+    ]
+    
 
-
-
-
-
-
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-});
+}
