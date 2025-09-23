7ff3afe08bd6819ab2ace16e81917a8d4cd89ccf
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-09-23 13:59:17 -0700

    added to server.js

---
+++ script.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import pug from 'pug';
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
