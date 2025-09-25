05df373b5664f9c3b153e234943ece34d137ea64
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-09-25 14:33:13 -0700

    got answer buttons to display and attempted to add next button and score counter

--- script.js
+++ script.js
@@ -43,11 +43,29 @@ window.onload = function()
         }        
     ]
     
+    let score = 0;
+
+    const nextButton = document.getElementById("nextButton");
+    const scoreDisplay = document.getElementById("score");
     const beginButton = document.getElementById("beginButton");
     const questionArea = document.getElementById("questionArea");
 
-    beginButton.onclick = () =>
+    beginButton.onclick = () => 
     {
+        beginButton.disabled = true;
+        loadQuestion();
+    }
+
+    nextButton.onclick = () => 
+    {
+        nextButton.style.display = "none";
+        loadQuestion();
+    }
+
+    function loadQuestion()
+    {
+        
+        
         questionArea.innerHTML = ""; //clear area of prev question
 
         const randomNum = Math.floor(Math.random() * (questions.length-1));
@@ -72,6 +90,8 @@ window.onload = function()
                 {
                     button.style.backgroundColor = "green";
                     alert("Correct!!!");
+                    score++;
+                    updateScore();
                 }
                 else
                 {
@@ -80,11 +100,16 @@ window.onload = function()
                 }
                 const allButtons = document.querySelectorAll(".answer");
                 allButtons.forEach(e => e.disabled = true);
-
-                questionArea.appendChild(button);
             }
+            questionArea.appendChild(button);
+            nextButton.style.display = "inline-block";
 
         });
+    }
+
+    function updateScore() 
+    {
+        scoreDisplay.innerText = `Current Score: ${score}`;
     }
 
     function shuffle(array) 
